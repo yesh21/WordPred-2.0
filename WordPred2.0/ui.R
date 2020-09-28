d238849a5d5227c1b8f092a2f50ac72490cf4a24
yaswanth Pulavarthi <sc19yp@leeds.ac.uk>
2020-09-28 17:36:15 +0100

    title

--- WordPred2.0/ui.R
+++ WordPred2.0/ui.R
@@ -3,12 +3,13 @@ library(dplyr)
 library(stringr)
 library(shinyWidgets)
 # Define UI for application that runs a word predictor
+
 shinyUI(fluidPage(theme = "input.css",
     
                   titlePanel(h1("WORD PREDICTOR", style="position: fixed;top: 20%;text-align: center;  
 color: white;
   text-shadow: 12px 12px 24px grey;
-  ")
+  "), windowTitle= "Word Predictor"
 ),
     fluidRow(
       
@@ -17,8 +18,13 @@ color: white;
       
                  textInput("inputString",  value = "have a",label = "Text:"),  
                             
-                      uiOutput("show")
+                      uiOutput("show"),
+                 tags$style(type="text/css",
+                            ".shiny-output-error { visibility: hidden; }",
+                            ".shiny-output-error:before { visibility: hidden; }"
+                 ),
                  
+                 icon("calendar")
                  
                   )
 
